878a8ff01911c148f78571c6b38f81c9b402ef76
Vlada Kuznietsova <168461338+val1dolum@users.noreply.github.com>
2024-05-10 09:51:34 +0300

    Update sum_even_nums_in_range.py

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,9 @@
 def sum_even_nums_in_range(start, stop):
-  # Type your code
-  return
+    sum_even = 0
+    for num in range(start, stop + 1):
+        if num % 2 == 0:
+            sum_even += num
+    return sum_even
+print(sum_even_nums_in_range(79, 42))
+print(sum_even_nums_in_range(25, 125))
+print(sum_even_nums_in_range(40, 70))
