9074aef6a73c03ba0ea46c7b3ade8059fd311150
Vlada Kuznietsova <168461338+val1dolum@users.noreply.github.com>
2024-05-10 09:44:35 +0300

    Update fizz_buzz.py

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,13 @@
-def fizz_buzz(param):
-  #Type your code here
-  return
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return str(number)
+print(fizz_buzz(27))
+print(fizz_buzz(25))
+print(fizz_buzz(120))
+print(fizz_buzz(26))
