34bfe822d063a8ca98c7005eb25244d3464095a7
Vlada Kuznietsova <168461338+val1dolum@users.noreply.github.com>
2024-05-10 09:51:07 +0300

    Update squares_sum.py

--- src/squares_sum.py
+++ src/squares_sum.py
@@ -1,3 +1,8 @@
 def squares_sum(n):
-  # Type your code
-  return
+    total_sum = 0
+    for i in range(1, n + 1):
+        total_sum += i * i
+    return total_sum
+print(squares_sum(25))
+print(squares_sum(42))
+print(squares_sum(8))
