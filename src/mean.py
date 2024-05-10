9a0c7326c43c744768c1df6adc44d38a807cbb83
Vlada Kuznietsova <168461338+val1dolum@users.noreply.github.com>
2024-05-10 09:52:01 +0300

    Update mean.py

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,8 @@
 def mean(number):
-  # Type your code
-  return 
+    num_str = str(number)
+    digit_sum = sum(int(digit) for digit in num_str)
+    mean_value = digit_sum // len(num_str)
+    return mean_value
+print(mean(25))
+print(mean(23102004))
+print(mean(777))
