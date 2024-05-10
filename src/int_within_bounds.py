c1163363b0ee61fd7e3ab1a1b6c96e19959af5b7
Vlada Kuznietsova <168461338+val1dolum@users.noreply.github.com>
2024-05-10 09:50:14 +0300

    Update int_within_bounds.py

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,3 +1,7 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-  # Type your code
-  return 
+    if not isinstance(number, int):
+        return False
+    return lower_bound <= number < upper_bound
+print(int_within_bounds(7, 4, 8))
+print(int_within_bounds(7, 7, 7))
+print(int_within_bounds(2.5, 3.5, 4.5))
