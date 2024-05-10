29122d09e9b805c1e9b850dbb33df2313fc365a5
Vlada Kuznietsova <168461338+val1dolum@users.noreply.github.com>
2024-05-10 09:49:40 +0300

    Update both.py

--- src/both.py
+++ src/both.py
@@ -1,3 +1,6 @@
 def both(number1, number2):
-  # Type your code
-  return
+    return (number1 < 0 and number2 < 0) or (number1 > 0 and number2 > 0) or (number1 == 0 and number2 == 0)
+print(both(2, -2))
+print(both(25, 25))
+print(both(0, 0))
+print(both(-77, 42))
